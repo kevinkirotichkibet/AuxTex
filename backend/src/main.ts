6adb831ad92c10686f65074987f9cdc51bf88ae8
kevinkirotichkibet <kirotichkevin@gmail.com>
2026-09-15 15:51:04 +0300

    Changed container port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.setGlobalPrefix('api');
   const port = process.env.PORT || 4000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Tailor API running on http://localhost:${port}/api`);
 }
 bootstrap();
